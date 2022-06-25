45655be0eac3d035298e0e55a5a09323288bc588
largado <alexandrohipolito@gmail.com>
2022-06-25 13:00:00 -0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+#Abre e cria arquivo
+arquivo = open("palavras.txt", "w")
+
+#escreve no arquivo
+arquivo.write("conteúdo do arquivo que o phyton escreveu\n")
+
+arquivo.write("linhas 2")
+
+#fecha o arquivo
+arquivo.close()
+
